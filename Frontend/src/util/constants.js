08231a572429da429316af2d0976ab834deb68ab
KASHISH SINGH <itskashish2004@gmail.com>
2025-09-24 07:45:32 +0530

    Update constants.js

--- Frontend/src/util/constants.js
+++ Frontend/src/util/constants.js
@@ -1,3 +1,5 @@
 export const AppConstants = {
-    BACKEND_URL: process.env.REACT_APP_API_URL 
+    BACKEND_URL: process.env.REACT_APP_API_URL
 };
+
+console.log("Using backend URL:", AppConstants.BACKEND_URL);
